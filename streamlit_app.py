87e8d02b431f455ed4fe34fbfbe997aa63a2b1d0
imchungi <107537424+imchungi@users.noreply.github.com>
2025-06-25 14:28:06 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title("🎈 My new app")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Let's 🎈 start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 st.write(
     "Hello Streamlit World"
